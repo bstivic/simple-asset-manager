8cd7c1191cc39ad5cf4c6f012cc3fb964b39b44b
Abdulloh Charis <charis@kapanlagi.net>
2017-03-09 13:17:25 +0700

    push release

---
+++ src/Serve.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use MatthiasMullie\Minify;
+
+class Serve
+{
+    var $path;
+    
+    public function __construct($path = null)
+    {
+        $this->path = $path;
+    }
+    
+    public function css(array $files)
+    {
+        header('Content-type: text/css');
+        $minify = new Minify\CSS();
+        $minify->add($files);
+        return $minify->minify($this->path);
+    }
+    
+    public function js(array $files)
+    {
+        header('Content-type: text/javascript');
+        $minify = new Minify\JS();
+        $minify->add($files);
+        
+        return $minify->minify($this->path);
+    }
+    
+    public function http_header($timeout = 604800, $maxAge = 315360000)
+    {
+        header("Expires: ". gmdate('D, d M Y H:i:s', time() + $timeout) ." GMT");
+        header("Last-Modified: ". gmdate('D, d M Y H:i:s', time() ) ." GMT");
+        header("Cache-Control: max-age=$maxAge");
+        header("Pragma: cache");
+        session_cache_limiter('public');
+    }
+}
